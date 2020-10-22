081938c536c8954a7ee7545a435dd0a833d93a5d
thamudi <tamim.hamoudi@gmail.com>
2020-10-22 17:13:09 +0300

    changed port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { response } = require('express');
 const express = require('express');
 const { request } = require('http');
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 app.use(express.static('./public'));
 
